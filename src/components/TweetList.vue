5f08b6955c58139a29b2a6d70728f5887bee4d79
Matt Priour <mpriour@esri.com>
2020-05-28 17:02:41 -0500

    Make tweet list respond to new tweets

--- src/components/TweetList.vue
+++ src/components/TweetList.vue
@@ -1,26 +1,15 @@
 <template>
     <ul class="list-plain">
         <li v-for="(tweet, i) in tweets" :key="`tweet-${i}`">
-            <p>{{tweet}}</p>
+            <p class="text-left panel panel-black">{{tweet}}</p>
         </li>
     </ul>
 </template>
 
 <script>
-import storage from '../api/storage.js'
-import memory from '../api/memory.js'
-debugger;
-const store = storage.available ? storage : memory;
-
 export default {
-    data(){
-        return {
-            tweets: []
-        }
-    },
-    mounted(){
-        this.tweets = store.get('tweets').split('|')
-    }
+    name: 'TweetList',
+    props: ['tweets'],
 }
 </script>
 
--- src/components/TwitterBox.vue
+++ src/components/TwitterBox.vue
@@ -1,17 +1,55 @@
 <template>
-  <entry-box />
-  <div class="panel panel-white">
-      <tweet-list />
-  </div>
+    <div class="panel panel-white">
+        <label for="tweetArea">Tweet</label>
+        <textarea name="tweetArea" id="tweetArea" :maxlength="maxLength" v-model="currentTweet"></textarea>
+        <button class="btn right leader-1" @click.prevent="saveTweet">Submit</button>
+        <div class="font-size--1 avenir-light text-left">
+            Characters remaining : <span>{{remaining}}</span>
+        </div>
+        <div class="alert alert-red is-active" v-if="remaining<0">Tweet exceeds the maximum limit and can't be saved</div>
+    </div>
+    <div class="panel panel-white">
+      <tweet-list :tweets="tweets" />
+    </div>
 </template>
 
 <script>
-import EntryBox from './EntryBox.vue'
 import TweetList from './TweetList.vue'
+import storage from '../api/storage.js'
+import memory from '../api/memory.js'
+
+const store = storage.available ? storage : memory;
+
 export default {
-  name: 'TwitterBox',
-  components:{
-      EntryBox, TweetList
-  }
+    name: 'TwitterBox',
+    components: { TweetList },
+    data(){
+        return {
+            currentTweet:'',
+            maxLength: 280,
+            tweets: []
+        }
+    },
+    computed:{
+        remaining(){
+            return this.maxLength - this.currentTweet.length
+        }
+    },
+    methods:{
+        saveTweet(evt){
+            if(!this.exceeds){
+                storage.append('tweets', this.currentTweet, '|')
+                this.tweets.push(this.currentTweet)
+                this.currentTweet = ''
+            }
+        }
+    },
+    mounted(){
+        this.tweets = store.get('tweets').split('|')
+    }
 }
 </script>
+
+<style>
+
+</style>
